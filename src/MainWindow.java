4a6840030194fef521f432c7b7f2d4ad191a37ce
Sébastien RIGOT <sebastien.rigot29@gmail.Com>
2017-11-17 16:41:35 +0100

    a

---
+++ src/MainWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame{
+    public MainWindow(){
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.setVisible(true);
+	this.setSize(800, 600);
+    }
+}
